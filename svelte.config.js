dec26bc2f1c11450652fa2e0b13047340f28a255
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-04-04 20:46:31 -0300

    HeroSection
    Criação do componente Hero

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
 
-const config = { kit: { adapter: adapter() } };
+const config = { kit: { 
+    adapter: adapter(), 
+    alias: {
+        'assets': "src/assets",
+        'components': "src/lib/components"
+    }
+} };
 
 export default config;
