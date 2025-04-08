17b30f4b79be947009427dbf9730141536552885
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-04-08 00:55:37 -0300

    Stripe
    Integração do Stripe como metodo de pagamento.

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,24 +1,43 @@
 <script>
-    let { children, ...props } = $props()
+  let { children, ...props } = $props();
+
+  import { loadStripe } from "@stripe/stripe-js";
+  import { PUBLIC_STRIPE_KEY } from "$env/static/public";
+
+  async function onclick() {
+    const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
+
+    const response = await fetch("/api/checkout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const { sessionId } = await response.json();
+    console.log({sessionId})
+
+    await stripe.redirectToCheckout({ sessionId });
+  }
 </script>
 
-<button {...props}>{@render children()}</button>
+<button {...props} {onclick}>
+  {@render children()}
+</button>
 
 <style>
-    button {
-        background-color: black;
-        color: white;
-        padding: 20px 24px;
-        font-weight: normal;
-        font-size: 22px;
-        text-transform: uppercase;
-        transition: all 0.3s;
-        border: 1px solid white;
-    }
+  button {
+    background-color: black;
+    color: white;
+    padding: 20px 24px;
+    font-weight: normal;
+    font-size: 22px;
+    text-transform: uppercase;
+    transition: all 0.3s;
+    border: 1px solid white;
+  }
 
-    button:hover {
-        background-color: white;
-        color: black;
-
-    }
+  button:hover {
+    background-color: white;
+    color: black;
+  }
 </style>
