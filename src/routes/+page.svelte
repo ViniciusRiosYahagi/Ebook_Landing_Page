8d8253a629b2a2696d77c04fe7a7836465efb476
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-04-09 00:27:13 -0300

    New Build

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { HeroSection, ChapterPreview, AuthorSection, FaqSection } from "components";
+</script>
+
+<HeroSection />
+<ChapterPreview />
+<AuthorSection />
+<FaqSection />
+
