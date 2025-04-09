d4e8f8ee4335e27ecc4a421c6f10917b5be31561
Vinicius Yahagi Rios <viniciusyahagirios@hotmail.com>
2025-04-09 01:20:33 -0300

    Project Complete

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import adapter from '@sveltejs/adapter-auto';
+
+const config = {
+	kit: {
+		adapter: adapter(),
+		alias: {
+			assets: 'src/assets',
+			components: 'src/lib/components'
+		}
+	}
+};
+
+export default config;
